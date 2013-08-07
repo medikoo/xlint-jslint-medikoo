0ba20c2e13da0669b6c8682e94654fe30f504bb6
Mariusz Nowak <mariusz@medikoo.com>
2013-08-07 14:24:05 +0200

    Update from master 2012-07-27

--- jslint.js
+++ jslint.js
@@ -1,5 +1,5 @@
 // jslint.js
-// 2012-07-24
+// 2012-07-27
 
 // Copyright (c) 2002 Douglas Crockford  (www.JSLint.com)
 
@@ -2636,7 +2636,7 @@ klass:              do {
         x.thru = 1;
         x.line = 0;
         x.edge = 'edge';
-        s.string = s;
+        x.string = s;
         return postscript(x);
     }
 
@@ -6559,7 +6559,7 @@ klass:              do {
 
     itself.jslint = itself;
 
-    itself.edition = '2012-07-24';
+    itself.edition = '2012-07-27';
 
     return itself;
 }());
